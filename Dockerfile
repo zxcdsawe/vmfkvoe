00348bcd8993a2bf38820f53f3b88a93a90647d9
zxcdsawe <99389759+zxcdsawe@users.noreply.github.com>
2023-03-05 07:36:31 +0000

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+﻿FROM alpine:latest
+EXPOSE 8080
+ADD entrypoint.sh /opt/entrypoint.sh
+
+RUN apk add --no-cache --virtual .build-deps ca-certificates curl \
+ && chmod +x /opt/entrypoint.sh
+
+ENTRYPOINT ["sh", "-c", "/opt/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+# Global variables
+DIR_CONFIG="/etc/v2ray"
+DIR_RUNTIME="/usr/bin"
+DIR_TMP="$(mktemp -d)"
+
+uuidone=0e8cc52e-caf5-44aa-90c1-697e49fd8887
+uuidtwo=56a697d8-3fcd-4cf0-859c-9ab1b5df58df
+uuidthree=fbcfc0ba-76ce-4fb6-a61a-ed239b70b802
+uuidfour=50da9c1f-bf7a-47fa-b933-f7ad52161d41
+uuidfive=281204fe-1c43-4d9d-8727-b2ba90fc9863
+mypath=/file-myfile
+myport=8080
+
+
+# Write V2Ray configuration
+cat << EOF > ${DIR_TMP}/myconfig.pb
+{
+	"inbounds": [
+		{
+			"listen": "0.0.0.0",
+			"port": $myport,
+			"protocol": "vless",
+			"settings": {
+				"decryption": "none",
+				"clients": [
+					{
+						"id": "$uuidone"
+					},
+					{
+						"id": "$uuidtwo"
+					},
+					{
+						"id": "$uuidthree"
+					},
+					{
+						"id": "$uuidfour"
+					},
+					{
+						"id": "$uuidfive"
+					}
+				]
+			
+		},
+		"streamSettings": {
+			"network": "ws",
+			"wsSettings": {
+					"path": "$mypath"
+				}
+			}
+		}
+	],
+	"outbounds": [
+		{
+			"protocol": "freedom"
+		}
+	]
+}
+EOF
+
+# Get V2Ray executable release
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
+unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+
+# Convert to protobuf format configuration
+mkdir -p ${DIR_CONFIG}
+mv -f ${DIR_TMP}/myconfig.pb ${DIR_CONFIG}/myconfig.json
+
+# Install V2Ray
+install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
+rm -rf ${DIR_TMP}
+
+# Run V2Ray
+${DIR_RUNTIME}/v2ray run -config=${DIR_CONFIG}/myconfig.json
